a8fa666ce20dfa2c644e91ad4afe5fc1f21cd8a6
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2016-10-19 16:03:00 +0200

    chore: webpack2 limits

---
+++ webpack.aot.js
@@ -0,0 +1,225 @@
+var path = require('path');
+var webpack = require('webpack');
+
+// Webpack Plugins
+var CommonsChunkPlugin = webpack.optimize.CommonsChunkPlugin;
+var autoprefixer = require('autoprefixer');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var CopyWebpackPlugin = require('copy-webpack-plugin');
+
+/**
+ * Env
+ * Get npm lifecycle event to identify the environment
+ */
+var ENV = process.env.MODE;
+var isProd = ENV === 'build';
+
+module.exports = function makeWebpackConfig() {
+  /**
+   * Config
+   * Reference: http://webpack.github.io/docs/configuration.html
+   * This is the object where all configuration gets set
+   */
+  var config = {};
+
+  /**
+   * Devtool
+   * Reference: http://webpack.github.io/docs/configuration.html#devtool
+   * Type of sourcemap to use per build type
+   */
+  if (isProd) {
+    config.devtool = 'source-map';
+  } else {
+    config.devtool = 'eval-source-map';
+  }
+
+  // add debug messages
+  config.debug = !isProd;
+
+  /**
+   * Entry
+   * Reference: http://webpack.github.io/docs/configuration.html#entry
+   */
+  config.entry = {
+    'app': './aot/demo/src/main' // our angular app
+  };
+
+  /**
+   * Output
+   * Reference: http://webpack.github.io/docs/configuration.html#output
+   */
+  config.output = {
+    path: root('aot'),
+    publicPath: isProd ? '/' : 'http://localhost:9090/',
+    filename: 'bundle.js',
+    //chunkFilename: isProd ? '[id].[hash].chunk.js' : '[id].chunk.js'
+  };
+
+  /**
+   * Resolve
+   * Reference: http://webpack.github.io/docs/configuration.html#resolve
+   */
+  config.resolve = {
+    root: root('demo'),
+    // only discover files that have those extensions
+    extensions: ['', '.ts', '.js', '.css', '.scss', '.html'],
+    alias: {
+      '@ng-bootstrap/ng-bootstrap': root('./aot/src/index')
+    }
+  };
+
+  /**
+   * Loaders
+   * Reference: http://webpack.github.io/docs/configuration.html#module-loaders
+   * List: http://webpack.github.io/docs/list-of-loaders.html
+   * This handles most of the magic responsible for converting modules
+   */
+  config.module = {
+    loaders: [
+      // Support for .ts files.
+      /*{
+        test: /\.ts$/,
+        loader: 'ts',
+        query: {
+          'ignoreDiagnostics': [
+            2403, // 2403 -> Subsequent variable declarations
+            2300, // 2300 -> Duplicate identifier
+            2374, // 2374 -> Duplicate number index signature
+            2375, // 2375 -> Duplicate string index signature
+            2502  // 2502 -> Referenced directly or indirectly
+          ]
+        },
+        exclude: [/node_modules\/(?!(ng2-.+))/]
+      },*/
+
+      /*{
+        test: /\.ts$/,
+        loader: 'angular2-template-loader'
+      },
+
+      // copy those assets to output
+      {test: /\.(png|jpe?g|gif|svg|woff|woff2|ttf|eot|ico)$/, loader: 'file?name=fonts/[name].[hash].[ext]?'},
+
+      // Support for CSS as raw text
+      // use 'null' loader in test mode (https://github.com/webpack/null-loader)
+      // all css in src/style will be bundled in an external css file
+      {
+        test: /\.css$/,
+        exclude: root('demo', 'src', 'app'),
+        loader: ExtractTextPlugin.extract('style', 'css?sourceMap!postcss')
+      },
+      // all css required in src/app files will be merged in js files
+      {test: /\.css$/, include: root('demo', 'src', 'app'), loader: 'raw!postcss'},
+
+      // support for .scss files
+      // use 'null' loader in test mode (https://github.com/webpack/null-loader)
+      // all css in src/style will be bundled in an external css file
+      {
+        test: /\.scss$/,
+        exclude: root('src', 'app'),
+        loader: ExtractTextPlugin.extract('style', 'css?sourceMap!postcss!sass')
+      },
+      // all css required in src/app files will be merged in js files
+      {test: /\.scss$/, exclude: root('demo', 'src', 'style'), loader: 'raw!postcss!sass'},
+
+      // support for .html as raw text
+      // todo: change the loader to something that adds a hash to images
+      {test: /\.html$/, loader: 'raw'},
+
+      {test: /\.md$/, loader: 'html!markdown'}*/
+    ],
+    postLoaders: [],
+    noParse: [/.+zone\.js\/dist\/.+/]
+  };
+
+  /**
+   * Plugins
+   * Reference: http://webpack.github.io/docs/configuration.html#plugins
+   * List: http://webpack.github.io/docs/list-of-plugins.html
+   */
+  config.plugins = [
+    // Define env variables to help with builds
+    // Reference: https://webpack.github.io/docs/list-of-plugins.html#defineplugin
+    new webpack.DefinePlugin({
+      // Environment helpers
+      'process.env': {
+        ENV: JSON.stringify(ENV),
+        version: JSON.stringify(require('./package.json').version)
+      }
+    }),
+
+   /* new CommonsChunkPlugin({
+      name: ['vendor', 'polyfills']
+    }),
+
+    // Inject script and link tags into html files
+    // Reference: https://github.com/ampedandwired/html-webpack-plugin
+    new HtmlWebpackPlugin({
+      template: './demo/src/public/index.html',
+      chunksSortMode: 'dependency'
+    }),
+
+    // Extract css files
+    // Reference: https://github.com/webpack/extract-text-webpack-plugin
+    // Disabled when in test mode or not in build mode
+    new ExtractTextPlugin('css/[name].[hash].css', {disable: !isProd})*/
+  ];
+
+  // Add build specific plugins
+  if (isProd) {
+    config.plugins.push(
+      // Reference: http://webpack.github.io/docs/list-of-plugins.html#noerrorsplugin
+      // Only emit files when there are no errors
+      new webpack.NoErrorsPlugin(),
+
+      // Reference: http://webpack.github.io/docs/list-of-plugins.html#dedupeplugin
+      // Dedupe modules in the output
+      //new webpack.optimize.DedupePlugin(),
+
+      // Reference: http://webpack.github.io/docs/list-of-plugins.html#uglifyjsplugin
+      // Minify all javascript, switch loaders to minimizing mode
+      new webpack.optimize.UglifyJsPlugin({
+        comments: false,
+        mangle: true,
+        sourceMap: true
+      })
+
+      // Copy assets from the public folder
+      // Reference: https://github.com/kevlened/copy-webpack-plugin
+      /*new CopyWebpackPlugin([{
+        from: root('demo/src/public')
+      }])*/
+    );
+  }
+
+  /**
+   * PostCSS
+   * Reference: https://github.com/postcss/autoprefixer-core
+   * Add vendor prefixes to your css
+   */
+  config.postcss = [
+    autoprefixer({
+      browsers: ['last 2 version']
+    })
+  ];
+
+  /**
+   * Dev server configuration
+   * Reference: http://webpack.github.io/docs/configuration.html#devserver
+   * Reference: http://webpack.github.io/docs/webpack-dev-server.html
+   */
+  config.devServer = {
+    contentBase: './demo/src/public',
+    historyApiFallback: true,
+    stats: 'minimal' // none (or false), errors-only, minimal, normal (or true) and verbose
+  };
+
+  return config;
+}();
+
+// Helper functions
+function root(args) {
+  args = Array.prototype.slice.call(arguments, 0);
+  return path.join.apply(path, [__dirname].concat(args));
+}
